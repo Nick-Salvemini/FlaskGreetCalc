17320cdd0b0def0e5bd741b5e719237e641340cd
Nick Salvemini <salvemini.nick@gmail.com>
2023-01-10 14:53:45 -0500

    completed greet/calc assignment

--- calc/app.py
+++ calc/app.py
@@ -2,38 +2,58 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.source('/add')
-def add():
-    '''
-    Add a and b
-    '''
-    a = request.args['a']
-    b = request.args['b']
-    return a + b
+"""Basic math operations."""
 
-@app.source('/sub')
-def sub():
-    '''
-    Subtract b from a
-    '''
-    a = request.args['a']
-    b = request.args['b']
-    return a - b
+# @app.route('/add')
+# def add():
+#     '''
+#     Add a and b
+#     '''
+#     a = request.args['a']
+#     b = request.args['b']
+#     return str(int(a) + int(b))
 
-@app.source('/mult')
-def mult():
-    '''
-    Multiply a and b
-    '''
-    a = request.args['a']
-    b = request.args['b']
-    return a * b
+# @app.route('/sub')
+# def sub():
+#     '''
+#     Subtract b from a
+#     '''
+#     a = request.args['a']
+#     b = request.args['b']
+#     return str(int(a) - int(b))
 
-@app.source('/div')
-def div():
+# @app.route('/mult')
+# def mult():
+#     '''
+#     Multiply a and b
+#     '''
+#     a = request.args['a']
+#     b = request.args['b']
+#     return str(int(a) * int(b))
+
+# @app.route('/div')
+# def div():
+#     '''
+#     Divide a and b
+#     '''
+#     a = request.args['a']
+#     b = request.args['b']
+#     return str(int(a) / int(b))
+
+# SOMEHOW, I FORGOT THAT THERE ARE BUILT IN METHODS FOR ADD, SUBTRACT, MULTIPLY, AND DIVIDE
+
+math = {
+    'add': lambda a, b: a+b,
+    'sub': lambda a, b: a-b,
+    'mult': lambda a, b: a*b,
+    'div': lambda a, b: a/b
+}
+
+@app.route('/math/<operation>')
+def do_math(operation):
     '''
-    Divide a and b
+    Performs the requested math operation with the input values of a and b
     '''
     a = request.args['a']
     b = request.args['b']
-    return a / b
+    return str(math[operation](int(a), int(b)))
--- calc/operations.py
+++ calc/operations.py
@@ -1,41 +1,23 @@
 from flask import Flask, request
 
-app = Flask(__name__)
-
 """Basic math operations."""
 
-@app.route('/add')
-def add():
-    '''
-    Add a and b
-    '''
-    a = request.args['a']
-    b = request.args['b']
+def add(a, b):
+    """Add a and b."""
+    
     return a + b
 
-@app.route('/sub')
-def sub():
-    '''
-    Subtract b from a
-    '''
-    a = request.args['a']
-    b = request.args['b']
+def sub(a, b):
+    """Substract b from a."""
+
     return a - b
 
-@app.route('/mult')
-def mult():
-    '''
-    Multiply a and b
-    '''
-    a = request.args['a']
-    b = request.args['b']
+def mult(a, b):
+    """Multiply a and b."""
+
     return a * b
 
-@app.route('/div')
-def div():
-    '''
-    Divide a and b
-    '''
-    a = request.args['a']
-    b = request.args['b']
+def div(a, b):
+    """Divide a by b."""
+
     return a / b
